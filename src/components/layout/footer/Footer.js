55c48300bc7b87f43dc250436403cf1d3332b619
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-12-11 00:04:20 +0300

    change email

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -3,17 +3,31 @@ import Social from '../social/Social';
 import './Footer.scss';
 
 const Footer = () => {
-    return (
-        <section className={'footer'} id='contact'>
-            <h1 className={'section-title'}>Contact</h1>
-            <div className={'contact-details'}>
-                <div><h1 className={'my-name'}>Cosmas Gikunju</h1></div>
-                <div><span>Full stack Developer</span></div>
-                <div><span>gikunjucosmas@gmail.com</span></div>
-                <Social />
-                <div><span className={'highlight'}>All rights reserved | &copy; {new Date().getFullYear()} | Cosmas Gikunju <span role="img" aria-label="alien">👽</span></span></div>
-            </div>
-        </section>
-    )
-}
+	return (
+		<section className={'footer'} id='contact'>
+			<h1 className={'section-title'}>Contact</h1>
+			<div className={'contact-details'}>
+				<div>
+					<h1 className={'my-name'}>Cosmas Gikunju</h1>
+				</div>
+				<div>
+					<span>Full stack Developer</span>
+				</div>
+				<div>
+					<span>devcosmas@gmail.com</span>
+				</div>
+				<Social />
+				<div>
+					<span className={'highlight'}>
+						All rights reserved | &copy; {new Date().getFullYear()}{' '}
+						| Cosmas Gikunju{' '}
+						<span role='img' aria-label='alien'>
+							👽
+						</span>
+					</span>
+				</div>
+			</div>
+		</section>
+	);
+};
 export default Footer;
