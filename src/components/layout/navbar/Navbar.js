b516cc92eb97450a1aec46e5f43b8ccbeda7e8ac
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-12-11 16:04:14 +0300

    Refactor to BEM

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -3,20 +3,38 @@ import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 const Navbar = () => {
-    return (
-        <div className={"navbar"}>
-            <div className={"navbar__logo-name"}>
-                <Link to="/" className="navbar__logo-link">Cozy</Link>
-            </div>
-            <div className={"navbar__nav"}>
-                <ul className={"navbar__list"}>
-                    <li className={"navbar__item"}><a href={'/#stack'} className={"navbar__link"}>Stack</a></li>
-                    <li className={"navbar__item"}><Link to={'projects'} className={"navbar__link"}>Projects</Link></li>
-                    <li className={"navbar__item"}><Link to={'blog'} className={"navbar__link"}>Blog</Link></li>
-                    <li className={"navbar__item"}><a href={'#contact'} className={"navbar__link"}>Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className={'navbar'}>
+			<div className={'navbar__logo-name text--azureRadiance'}>
+				<Link to='/' className='navbar__logo-link'>
+					Cozy
+				</Link>
+			</div>
+			<div className={'navbar__nav'}>
+				<ul className={'navbar__list text--azureRadiance'}>
+					<li className={'navbar__list--item'}>
+						<a href={'/#stack'} className={'navbar__list--link'}>
+							Stack
+						</a>
+					</li>
+					<li className={'navbar__list--item'}>
+						<Link to={'projects'} className={'navbar__list--link'}>
+							Projects
+						</Link>
+					</li>
+					<li className={'navbar__list--item'}>
+						<Link to={'blog'} className={'navbar__list--link'}>
+							Blog
+						</Link>
+					</li>
+					<li className={'navbar__list--item'}>
+						<a href={'#contact'} className={'navbar__list--link'}>
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 export default Navbar;
