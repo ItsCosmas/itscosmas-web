c44de6dfe2c39659c3fb514c2b044fec1f70ad8d
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-12-11 16:00:44 +0300

    refactoring to BEM

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -5,20 +5,22 @@ import './Footer.scss';
 const Footer = () => {
 	return (
 		<section className={'footer'} id='contact'>
-			<h1 className={'section-title'}>Contact</h1>
-			<div className={'contact-details'}>
-				<div>
-					<h1 className={'my-name'}>Cosmas Gikunju</h1>
-				</div>
-				<div>
-					<span>Full stack Developer</span>
-				</div>
-				<div>
-					<span>devcosmas@gmail.com</span>
-				</div>
+			<h1 className={'heading__primary heading__primary--prussianBlue'}>
+				Contact
+			</h1>
+			<div className={'contact__details'}>
+				<h4 className={'heading__tertiary text--prussianBlue'}>
+					Cosmas Gikunju
+				</h4>
+				<span className={'heading__tertiary text--prussianBlue'}>
+					Full Stack Developer
+				</span>
+				<span className='heading__tertiary text--prussianBlue u-margin-bottom-small'>
+					devcosmas@gmail.com
+				</span>
 				<Social />
-				<div>
-					<span className={'highlight'}>
+				<div className={'u-margin-top-small'}>
+					<span className={'text__highlight'}>
 						All rights reserved | &copy; {new Date().getFullYear()}{' '}
 						| Cosmas Gikunju{' '}
 						<span role='img' aria-label='alien'>
